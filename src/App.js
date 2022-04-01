2f1c3e1e48a6b5791d1213f9333c7021bdeea11d
Britney Wu <Wu.Britney@gmail.com>
2022-04-01 13:09:12 -0400

    added error 404 page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound';
 import Footer from './components/layout/Footer';
 import './index.css'
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col justify-between h-screen">    
+      <div className='flex flex-col justify-between h-screen'>    
         <Navbar />
-          <main className='container mx-auto px-3 pb-12'>Main content</main>
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/' element={<About />} />
+              <Route path='/' element={<NotFound />} />
+              <Route path='/*' element={<NotFound />} />
+
+            </Routes>
+          </main>
           <Footer />
       </div>
     </Router>
